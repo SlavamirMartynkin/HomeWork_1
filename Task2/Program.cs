b8c62de5498bb2da38f60152c73e5298549b6dc7
Martynkin Slavamir <sss_mmm86@mail.ru>
2023-04-07 23:29:17 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Введите первое число ");
+string s = Console.ReadLine();
+int i = int.Parse(s);
+
+System.Console.WriteLine("Введите Второе число ");
+string t = Console.ReadLine();
+int j = int.Parse(t);
+
+if (i>j)
+{
+    System.Console.WriteLine($"Большее число {i}");
+    System.Console.WriteLine($"Меньшее число {j}");
+}
+else if (j>i)
+{
+    System.Console.WriteLine($"Большее число {j}");
+    System.Console.WriteLine($"Меньшее число {i}");
+}
+else
+{
+    System.Console.WriteLine("Числа равны");
+}
